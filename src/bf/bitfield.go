bb4d3f94f3cb33f0612245839dd480f356f4c279
smigii <smigii@protonmail.com>
2022-06-28 16:41:59 -0500

    Refactor Bitfield to use a second byte slice
    I must have forgotten how byte slices work when writing this.
    
    * data5 points to 5 prepended bytes + bitfield
    * data points to bitfield

--- src/bf/bitfield.go
+++ src/bf/bitfield.go
@@ -6,11 +6,12 @@ import "fmt"
 // only holding the data of the bitfield, the first 5 bytes represent the length
 // (4 bytes) and id (1 byte) of a bitfield message. This is so we are not
 // wasting time prepending 5 bytes to a potentially long slice every time we need
-// to send a
+// to send a bitfield message.
 type Bitfield struct {
-	data5  []byte // Holds the first 5 bytes + actual data
-	nbits  int64
+	data5  []byte // Holds the first 5 bytes + bitfield data
+	data   []byte // Holds only the bitfield data
 	nbytes int64
+	nbits  int64
 	nset   int64
 }
 
@@ -20,8 +21,11 @@ func NewBitfield(nbits int64) *Bitfield {
 		nbytes += 1
 	}
 
+	data5 := make([]byte, 5+nbytes, 5+nbytes)
+
 	bf := Bitfield{
-		data5:  make([]byte, 5+nbytes, 5+nbytes),
+		data5:  data5,
+		data:   data5[5:],
 		nbits:  nbits,
 		nbytes: nbytes,
 		nset:   0,
@@ -47,6 +51,7 @@ func FromBytes(bytes5 []byte, nbits int64) (*Bitfield, error) {
 
 	bf := Bitfield{
 		data5:  bytes5,
+		data:   bytes5[5:],
 		nbits:  nbits,
 		nbytes: lenbytes - 5,
 		nset:   0,
@@ -61,7 +66,7 @@ func FromBytes(bytes5 []byte, nbits int64) (*Bitfield, error) {
 func (bf *Bitfield) calcNset() {
 	var i int64
 	const maskStart = uint8(128)
-	last := int64(len(bf.data5) - 1)
+	last := int64(len(bf.data) - 1)
 	rem := bf.nbits % 8
 	nset := int64(0)
 
@@ -71,10 +76,10 @@ func (bf *Bitfield) calcNset() {
 		last++
 	}
 
-	for i = 5; i < last; i++ {
+	for i = 0; i < last; i++ {
 		for j := 0; j < 8; j++ {
 			mask := maskStart >> j
-			if (bf.data5[i] & mask) > 0 {
+			if (bf.data[i] & mask) > 0 {
 				nset++
 			}
 		}
@@ -85,7 +90,7 @@ func (bf *Bitfield) calcNset() {
 	// if nbits % 8 == 0
 	for i = 0; i < rem; i++ {
 		mask := maskStart >> i
-		if (bf.data5[last] & mask) > 0 {
+		if (bf.data[last] & mask) > 0 {
 			nset++
 		}
 	}
@@ -105,7 +110,7 @@ func (bf *Bitfield) Get(idx int64) bool {
 	min := idx % 8
 	mask := uint8(128) >> min
 
-	elem := bf.data5[maj+5]
+	elem := bf.data[maj]
 	return (elem & mask) > 0
 }
 
@@ -113,15 +118,15 @@ func (bf *Bitfield) Set(idx int64, val bool) {
 	maj := idx / 8
 	min := idx % 8
 	getMask := uint8(128) >> min
-	isSet := (bf.data5[maj+5] & getMask) > 0
+	isSet := (bf.data[maj] & getMask) > 0
 
 	if !isSet && val {
 		mask := uint8(128) >> min
-		bf.data5[maj+5] |= mask
+		bf.data[maj] |= mask
 		bf.nset++
 	} else if isSet && !val {
 		mask := ^(uint8(128) >> min)
-		bf.data5[maj+5] &= mask
+		bf.data[maj] &= mask
 		bf.nset--
 	}
 }
@@ -130,6 +135,10 @@ func (bf *Bitfield) Set(idx int64, val bool) {
 // represent the length (4 bytes) and id (1 byte)
 func (bf *Bitfield) Data5() []byte {
 	return bf.data5
+}
+
+func (bf *Bitfield) Data() []byte {
+	return bf.data
 }
 
 func (bf *Bitfield) Complete() bool {
